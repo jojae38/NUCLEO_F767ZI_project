d4f23e04c33bb0cf9e237db670dfaddeaaa03478
jojaehyun <jojae38@naver.com>
2026-02-06 10:11:42 +0900

    수정

--- 2.firmware/Comp/service/cli.c
+++ 2.firmware/Comp/service/cli.c
@@ -747,63 +747,12 @@ void cliShowList(cli_args_t *args)
 
 void cliBnoSend(cli_args_t *args)
 {
-  uint8_t buffer;
-  char *endptr;
 
-  if (args->argc != 2)
-  {
-      cliPrintf("Usage: rfid send <hex>\r\n");
-      return;
-  }
-
-  unsigned long val2 = strtoul(args->argv[1], &endptr, 16); // ★ base=16 고정
-
-  if (*endptr != '\0' || val2 > 0xFF)
-  {
-      cliPrintf("Invalid hex\r\n");
-      return;
-  }
-
-  buffer = (uint8_t)val2;
-
-//  bno085SpiTransmit(&buffer, 1);
 }
 
 void cliBnoRead(cli_args_t *args)
 {
-  uint8_t tmp_addr;
-  uint32_t len;
-  uint8_t buffer[4096];
-  memset(buffer,0,4096);
-  char *endptr;
 
-  if (args->argc != 2)
-  {
-      cliPrintf("Usage: rfid send <hex>\r\n");
-      return;
-  }
-
-
-  unsigned long val = strtoul(args->argv[0], &endptr, 16); // ★ base=16 고정
-
-  if (*endptr != '\0' || val > 0xFF)
-  {
-      cliPrintf("Invalid hex\r\n");
-      return;
-  }
-  tmp_addr = (uint8_t)val;
-  len = atoi(args->argv[1]);
-
-//  bno085SpiTransmitReceive(&tmp_addr, 1, buffer, len);
-
-  for(int i = 0; i< len; i++)
-  {
-    if(i%10 == 0)
-    {
-      cliPrintf("\n ");
-    }
-    cliPrintf("0x%02X ",buffer[i]);
-  }
 }
 
 //void cliMemoryDump(cli_args_t *args)
